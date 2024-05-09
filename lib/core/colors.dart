979ea43a06804f94da8a3b240457681436cd2e0f
Mohammed <mohjalab9@gamil.com>
2024-05-09 18:28:54 +0300

    build the structure of the project

---
+++ lib/core/colors.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xff246bfd);
+const Color secondaryColor = Color(0xffffd300);
+const Color successColor = Color(0xff4ade80);
+const Color infoColor = Color(0xff246bfd);
+const Color warningColor = Color(0xfffacc15);
+const Color errorColor = Color(0xfff75555);
+const Color disabled1Color = Color(0xffd8d8d8);
+const Color disabled2Color = Color(0xff3062c8);
+const Color greyscale900Color = Color(0xff212121);
+const Color greyscale800Color = Color(0xff424242);
+const Color greyscale700Color = Color(0xff616161);
+const Color greyscale600Color = Color(0xff757575);
+const Color greyscale500Color = Color(0xffe9e9e9);
+const Color greyscale400Color = Color(0xffbdbdbd);
+const Color greyscale300Color = Color(0xffe0e0e0);
+const Color greyscale200Color = Color(0xffeeeeee);
+const Color greyscale100Color = Color(0xfff5f5f5);
+const Color greyscale50Color = Color(0xfffafafa);
+
+// gradient blue
+// gradient yellow
+// gradient green
+// gradient orange
+// gradient red
+
+const Color dark1Color = Color(0xff181a20);
+const Color dark2Color = Color(0xff1f222a);
+const Color dark3Color = Color(0xff35383f);
---
+++ lib/core/themes.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+
+const TextStyle headline1 =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 48);
+const TextStyle headline2 =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 40);
+const TextStyle headline3 =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 32);
+const TextStyle headline4 =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 24);
+const TextStyle headline5 =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 20);
+const TextStyle headline6 =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 18);
+
+const TextStyle bodyXLarge1 =
+    TextStyle(fontWeight: FontWeight.w700, fontSize: 18);
+const TextStyle bodyXLarge2 =
+    TextStyle(fontWeight: FontWeight.w600, fontSize: 18);
+const TextStyle bodyXLarge3 =
+    TextStyle(fontWeight: FontWeight.w500, fontSize: 18);
+const TextStyle bodyXLarge4 =
+    TextStyle(fontWeight: FontWeight.w400, fontSize: 18);
+
+const TextStyle bodyLarge1 =
+    TextStyle(fontWeight: FontWeight.w700, fontSize: 16);
+const TextStyle bodyLarge2 =
+    TextStyle(fontWeight: FontWeight.w600, fontSize: 16);
+const TextStyle bodyLarge3 =
+    TextStyle(fontWeight: FontWeight.w500, fontSize: 16);
+const TextStyle bodyLarge4 =
+    TextStyle(fontWeight: FontWeight.w400, fontSize: 16);
+
+const TextStyle bodyMedium1 =
+    TextStyle(fontWeight: FontWeight.w700, fontSize: 14);
+const TextStyle bodyMedium2 =
+    TextStyle(fontWeight: FontWeight.w600, fontSize: 14);
+const TextStyle bodyMedium3 =
+    TextStyle(fontWeight: FontWeight.w500, fontSize: 14);
+const TextStyle bodyMedium4 =
+    TextStyle(fontWeight: FontWeight.w400, fontSize: 14);
+
+const TextStyle bodySmall1 =
+    TextStyle(fontWeight: FontWeight.w700, fontSize: 12);
+const TextStyle bodySmall2 =
+    TextStyle(fontWeight: FontWeight.w600, fontSize: 12);
+const TextStyle bodySmall3 =
+    TextStyle(fontWeight: FontWeight.w500, fontSize: 12);
+const TextStyle bodySmall4 =
+    TextStyle(fontWeight: FontWeight.w400, fontSize: 12);
+
+const TextStyle bodyXSmall1 =
+    TextStyle(fontWeight: FontWeight.w700, fontSize: 10);
+const TextStyle bodyXSmall2 =
+    TextStyle(fontWeight: FontWeight.w600, fontSize: 10);
+const TextStyle bodyXSmall3 =
+    TextStyle(fontWeight: FontWeight.w500, fontSize: 10);
+const TextStyle bodyXSmall4 =
+    TextStyle(fontWeight: FontWeight.w400, fontSize: 10);
+
+// ThemeData lightTheme = ThemeData(
+//   fontFamily: 'Urbanist',
+//   textTheme: const TextTheme(
+//     // displayLarge instead of headline1
+//     displayLarge: TextStyle(fontWeight: FontWeight.bold, fontSize: 48),
+//     // displayMedium is headline2
+//     displayMedium: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+//     // displaySmall is headline3
+//     displaySmall: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+//     // headlineMedium is headline4
+//     headlineMedium: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+//     // headlineSmall is headline5
+//     headlineSmall: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+//     // titleLarge is headline6
+//     titleLarge: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+//     // titleMedium is bodyXLarge
+//     titleMedium: TextStyle(),
+//     // titleSmall is bodyLarge
+//     titleSmall: TextStyle(),
+//     // bodyLarge is bodyMedium
+//     bodyLarge: TextStyle(),
+//     // bodyMedium is bodySmall
+//     bodyMedium: TextStyle(),
+//     // bodySmall is bodyXSmall
+//     bodySmall: TextStyle(),
+//   ),
+// );
