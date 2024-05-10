721f7258bfc0be3851e40812c8fed7165932d88d
Mohammed <mohjalab9@gamil.com>
2024-05-10 20:07:22 +0300

    create buttons, images and colors

--- lib/main.dart
+++ lib/main.dart
@@ -15,54 +15,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Hichat',
       home: const Scaffold(),
-      // const TestPage(),
     );
   }
 }
-
-// class TestPage extends StatelessWidget {
-//   const TestPage({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SafeArea(
-//       child: Scaffold(
-//         body: Center(
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: <Widget>[
-//               const Text('Start'),
-//               const SizedBox(height: 10),
-//               DefaultButton(
-//                 enabled: false,
-//                 title: "Get Started",
-//                 onPressed: () {},
-//                 backgroundColor: dark3Color,
-//                 foregroundColor: whiteColor,
-//                 disabledBackgroundColor: dark2Color,
-//                 disabledForegroundColor: greyscale100Color,
-//               ),
-//               const SizedBox(height: 30),
-//               DefaultButtonWithIcon(
-//                 icon: FontAwesomeIcons.cartShopping,
-//                 // enabled: false,
-//                 title: "Get Started",
-//                 onPressed: () {},
-//                 backgroundColor: blueBackground,
-//                 foregroundColor: infoColor,
-//                 disabledBackgroundColor: dark2Color,
-//                 disabledForegroundColor: greyscale100Color,
-//               ),
-//               const SizedBox(height: 30),
-//               SocialButton(
-//                   isDark: false,
-//                   title: 'Continue with Apple',
-//                   logo: Logo.apple,
-//                   onPressed: () {})
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
